70af61af2826999e32314ec5b8665d8a009a0f32
Manooonlb <[manonlambert.lb@gmail.com](mailto:manonlambert.lb@gmail.com)>
2023-04-25 12:15:21 +0200

    payment issue

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 require_once __DIR__ . '/../config/debug.php';
@@ -8,3 +9,4 @@ if (file_exists(__DIR__ . '/../config/db.php')) {
 }
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../src/routing.php';
+
