85b69c2689309d2c3dbafa81736dba3d9b227426
Manooonlb <[manonlambert.lb@gmail.com](mailto:manonlambert.lb@gmail.com)>
2023-04-25 15:48:04 +0200

    Add to cart ok

--- src/Model/CartManager.php
+++ src/Model/CartManager.php
@@ -2,59 +2,42 @@
 
 namespace App\Model;
 
-
 class CartManager extends AbstractManager
 {
-
     /**
      * Add a new item in the cart
      */
     public function add(int $id)
     {
-
-        $itemManager = new ItemManager();
-
-        if (!isset($_SESSION))
-        {
+        if (!isset($_SESSION)) {
             session_start();
         }
-        if (!isset($_SESSION['cart']))
-        {
+        if (!isset($_SESSION['cart'])) {
             $_SESSION['cart'] = [];
         }
-        if (isset($_GET['id']))
-        {
+        if (isset($_GET['id'])) {
             $id = $_GET['id'];
         }
-         // récupérer le produit en BD
-         $item = $itemManager->selectOneById($id);
-         
-        if (isset($_SESSION['cart'][$id]))
-        {
-            $_SESSION['cart'][$id]++;   
-        } else {
-            $_SESSION['cart'][$id] = 1; 
-        }
-        // var_dump($_SESSION['cart']);
-        // // Récupérer le panier depuis la session ou le créer s'il n'existe pas encore
-        // // $cart = $request->getSession()->get('cart', []);
-        // if(!isset($_SESSION['cart']))
-        // {
-        //     $_SESSION['cart'] = [];
-        // } 
-        // $cart = $_SESSION['cart'];
 
-        // // Ajouter le produit dans le panier
-        // if (isset($cart[$id])) {
-        //     $cart[$id]['quantity']++;
+        // récupérer le produit en BD
+        $itemManager = new ItemManager();
+        $item = $itemManager->selectOneById($id);
+        $quantity = $_SESSION['cart'][$id]['quantity'] ?? 0;
+        $_SESSION['cart'][$id] = [
+            'quantity' => 1,
+            'name' => $item['name'],
+            'price' => $item['price'],
+            'quantity' => $quantity + 1
+        ];
+        // if (isset($_SESSION['cart'][$id])) {
+        //     var_dump($_SESSION, $id, $item);die;
+        //     $_SESSION['cart'][$id]['quantity']++;
         // } else {
-        //     $cart[$id] = [
-        //         'product' => $item,
+        //     $_SESSION['cart'][$id] = [
         //         'quantity' => 1,
+        //         'name' => $item['name'],
+        //         'price' => $item['price']
         //     ];
-
-        //     // Stocker le panier dans la session
-        //     // $request->getSession()->set('cart', $cart);
         // }
     }
 }
