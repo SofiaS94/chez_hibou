551ad9eb5e353c2a4481652eccc5c4849ef668aa
Nour <nourchaouch44@gmail.com>
2023-04-26 12:31:23 +0200

    images base de donnee

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -20,16 +20,15 @@ class ItemController extends AbstractController
     /**
      * List all items dans food
      */
-    public function listByType(string $typeName = 'entree'): string
+    public function listByType(string $type = 'entree'): string
     {
         // Récupérer le type de plat sélectionné depuis les paramètres de requête HTTP
-        $typeName = $_GET['type'] ?? 'entree'; // Utiliser 'entree' comme valeur par défaut
 
         $itemManager = new ItemManager();
-        $items = $itemManager->selectByType($typeName);
+        $items = $itemManager->selectByType($type);
 
         // Renvoyer la réponse sous forme de vue générée avec les items récupérés
-        return $this->twig->render('Item/' . $typeName . '.html.twig', ['items' => $items]);
+        return $this->twig->render('Item/' . $type . '.html.twig', ['items' => $items]);
     }
 }
 
