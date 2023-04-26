3908d47007a5bfb27c09d25ed1c56c52c313bc27
Manooonlb <[manonlambert.lb@gmail.com](mailto:manonlambert.lb@gmail.com)>
2023-04-26 09:40:53 +0200

    ajout compteur panier + bon de reduction

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -9,32 +9,25 @@ class CartController extends AbstractController
 {
     public function show(?string $code = null)
     {
+        $cart = $_SESSION['cart'] ?? [];
+
         $totalReduction = 0;
         $amount = 0;
-
-        $cart = $_SESSION['cart'] ?? [];
-        if (!empty($_SESSION['cart'])) {
-            foreach ($_SESSION['cart'] as $product) {
-                $amount += $product['price'] * $product['quantity'];
-            }
-
-            if ($code !== null && (empty($_SESSION['reductions']) || $code !== $_SESSION['reductions']['code'])) {
-                $reductionManager = new ReductionManager();
-                $_SESSION['reductions'] = $reductionManager->findReductions($code);
-            }
-            if (!empty($_SESSION['reductions'])) {
-                $totalReduction = $amount * $_SESSION['reductions']['pourcentage'] / 100;
-            }
-
-            return $this->render('cart/cart.html.twig', [
-                'cart' => $cart, 'montant' => $amount,
-                'totalReduction' => $totalReduction
-            ]);
-        } else {
-            return $this->render('Cart/cart.html.twig');
+        foreach ($_SESSION['cart'] as $product) {
+            $amount += $product['price'] * $product['quantity'];
         }
-    }
 
+        if ($code !== null && (empty($_SESSION['reductions']) || $code !== $_SESSION['reductions']['code'])) {
+            $reductionManager = new ReductionManager();
+            $_SESSION['reductions'] = $reductionManager->findReductions($code);
+        }
+        if (!empty($_SESSION['reductions'])) {
+            $totalReduction = $amount * $_SESSION['reductions']['pourcentage'] / 100;
+        }
+
+        return $this->render('cart/cart.html.twig', ['cart' => $cart, 'montant' => $amount,
+        'totalReduction' => $totalReduction]);
+    }
 
 
 
